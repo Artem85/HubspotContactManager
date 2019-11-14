175fa75bdc6255f6e0e8792e7f362131e1af7604
Zainchkovskyy Artem <zainchkovskyy.artem@gmail.com>
2019-11-14 03:39:33 +0200

    Generate excel file

--- ContactsManager/Controllers/HomeController.cs
+++ ContactsManager/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using ContactsManager.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace ContactsManager.Controllers
 {
@@ -32,11 +38,58 @@ namespace ContactsManager.Controllers
             long startDateUnix = (long)sDate.Subtract((new DateTime(1970, 1, 1))).TotalMilliseconds;
             long endDateUnix = (long)eDate.Subtract((new DateTime(1970, 1, 1))).TotalMilliseconds;
 
-            var contacts = ContactsData.GetContactsData(settings)
-                    .Where(t => { long unixDate = Int64.Parse(t.LastModifiedDate);
-                                    return startDateUnix <= unixDate && unixDate <= endDateUnix; });
+            var contacts = GetContactsListForGivenDateRange(startDateUnix, endDateUnix);
 
-            return View(contacts);
+            return GenerateExcelFile(contacts);
+
+            //return View(contacts);
+        }
+
+        private List<ContactsDataItem> GetContactsListForGivenDateRange (long startDateUnix, long endDateUnix)
+        {
+            return ContactsData.GetContactsData(settings)
+                    .Where(t => {
+                        long unixDate = Int64.Parse(t.LastModifiedDate);
+                        return startDateUnix <= unixDate && unixDate <= endDateUnix;
+                    }).ToList();
+        }
+
+        public IActionResult GenerateExcelFile(List<ContactsDataItem> contacts)
+        {
+            var memoryStream = new MemoryStream();
+            string fileName = "report.xlsx";
+
+            using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Contacts");
+
+                PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(ContactsDataItem));
+
+                IRow row = excelSheet.CreateRow(0);
+                for (int i = 0; i < properties.Count; i++)
+                {
+                    row.CreateCell(i).SetCellValue(properties[i].Name);
+                }
+
+                for (int i = 0; i < contacts.Count; i++)
+                {
+                    row = excelSheet.CreateRow(i + 1);
+                    for (int j = 0; j < properties.Count; j++)
+                    {
+                        row.CreateCell(j).SetCellValue(properties[j].GetValue(contacts[i]).ToString());
+                    }
+                }
+                workbook.Write(fileStream);
+            }
+
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                stream.CopyTo(memoryStream);
+            }
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
         }
 
     }
